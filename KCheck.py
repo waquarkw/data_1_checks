063f2cf6bea261ec84a3f8bc757a68328aceed01
Khalid Waquar <thisiskhalid76@gmail.com>
2023-03-14 16:40:18 -0400

    Completed Knowledge Check 1

---
+++ KCheck.py
@@ -0,0 +1,31 @@
+## KNOWLEDGE CHECK 1
+
+# 1. Write a statement that prints "hello world!"
+
+print('hello world')
+
+
+# 2. Write a code to create a list populated with several values. 
+# They can be strings / integers / floats etc. Then, print() one of
+#  those values either to the prompt or in a cell
+
+mylist = [25, 40, 55, 60, 75, 90, 105, 130, 155, 180];
+print("3rd element =", mylist[2])
+
+
+
+# 3. Write a code to create a dictionary populated with two keys and two values. 
+# Print one of the values just like you did above.
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car["brand"])
+
+
+# 4.Write a code to create a tuple with 4 values. Print one of them.
+
+top_players = ("Ronaldo", "Messi", "Neymar", "Mbappe")
+print("2nd element is:", top_players[1])
